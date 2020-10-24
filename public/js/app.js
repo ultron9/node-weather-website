06312e6ef0c8fd6b494f4f1ddada89105910dc37
unknown <anjantewani927@gmail.com>
2020-10-24 17:52:41 +0530

    Update index-hbs page

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ const form = document.querySelector('form')
 const search = document.querySelector('input')
 const paraOne = document.querySelector('#paraOne')
 const paraTwo = document.querySelector('#paraTwo')
-const paraThree = document.querySelector('#paraThree')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -15,9 +14,8 @@ form.addEventListener('submit', (e) => {
             if(data.error) {
                 paraOne.textContent = data.error
             }else {
-                paraOne.textContent = data.city
-                paraTwo.textContent = `${data.data.temperature} Celsius. ${data.data.forecast}`
-                paraThree.textContent = `Wind speed is ${data.data.wind} kmph`
+                paraOne.textContent = `${data.data.temperature} Celsius. ${data.data.forecast}`
+                paraTwo.textContent = `Wind speed is ${data.data.wind} kmph`
             }
         //    console.log(data.location.name)
         //    console.log(data.current.weather_descriptions[0])
